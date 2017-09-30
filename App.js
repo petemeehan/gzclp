71c7c343357ab60e832b78a9a9e2249931705fdf
apmeehan <apetermeehan@gmail.com>
2017-09-30 03:05:54 +0100

    REFACTOR (Created Lift component)
    Reduces much repetition, as each tier (T1/T2/T3) has its own fixed set
    of rep schemes

--- App.js
+++ App.js
@@ -34,6 +34,47 @@ class HomeScreen extends React.Component {
 const DEVICE_W = Dimensions.get('window').width;
 const DEVICE_H = Dimensions.get('window').height;
 
+const REP_SCHEMES = {
+  T1: {
+    1: {
+      sets: 5,
+      reps: 3,
+    },
+    2: {
+      sets: 6,
+      reps: 2,
+    },
+    3: {
+      sets: 10,
+      reps: 1,
+    },
+    amrap: true,
+  },
+  T2: {
+    1: {
+      sets: 3,
+      reps: 10,
+    },
+    2: {
+      sets: 3,
+      reps: 8,
+    },
+    3: {
+      sets: 3,
+      reps: 6,
+    },
+    amrap: false,
+  },
+  T3: {
+    1: {
+      sets: 3,
+      reps: 15,
+    },
+    amrap: true,
+  },
+}
+
+
 class SetButton extends React.Component {
   constructor(props) {
     super(props);
@@ -53,7 +94,8 @@ class SetButton extends React.Component {
       styles.setButtonSuccess : styles.setButtonBlank;
     let currentTextStyle = this.state.pressStatus ?
       styles.setButtonTextSuccess : styles.setButtonTextBlank;
-    let currentText = this.state.pressStatus ? '✓' : this.props.reps;
+    let currentText = this.state.pressStatus ?
+      '✓' : this.props.reps + (this.props.amrap ? '+' : '');
     return (
       <TouchableOpacity
         style={currentStyle}
@@ -62,6 +104,32 @@ class SetButton extends React.Component {
         <Text style={currentTextStyle}>{currentText}</Text>
       </TouchableOpacity>
     )
+  }
+}
+
+
+class Lift extends React.Component {
+  render() {
+    let sets = REP_SCHEMES[this.props.tier][this.props.repScheme].sets;
+    let reps = REP_SCHEMES[this.props.tier][this.props.repScheme].reps;
+    let amrap = REP_SCHEMES[this.props.tier].amrap;
+
+    // Populate an array of SetButtons for display, and if the rep scheme calls
+    // for an AMRAP final set, represent that in the final button in the array
+    let setButtons = [];
+    for (var i = 0; i < sets - 1; i++) {
+      setButtons.push(<SetButton reps={reps} key={i} />);
+    }
+    setButtons.push(<SetButton reps={reps} amrap={amrap} key={i} />);
+
+    return (
+      <View>
+        <Text style={styles.liftName}>{this.props.tier} {this.props.exercise}</Text>
+        <View style={styles.setButtonContainer}>
+          {setButtons}
+        </View>
+      </View>
+    );
   }
 }
 
@@ -75,28 +143,9 @@ class WorkoutA1 extends React.Component {
     const {navigate} = this.props.navigation;
     return (
       <ScrollView style={styles.container}>
-        <Text style={styles.liftName}>T1 Squat</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3+' />
-        </View>
-
-        <Text style={styles.liftName}>T2 Bench</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='10' />
-          <SetButton reps='10' />
-          <SetButton reps='10' />
-        </View>
-
-        <Text style={styles.liftName}>T3 Lat Pulldown</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='15' />
-          <SetButton reps='15' />
-          <SetButton reps='15+' />
-        </View>
+        <Lift tier='T1' repScheme='1' exercise='Squat' />
+        <Lift tier='T2' repScheme='1' exercise='Bench' />
+        <Lift tier='T3' repScheme='1' exercise='Lat Pulldown' />
 
         <Button
           onPress={() => navigate('B1')}
@@ -115,28 +164,9 @@ class WorkoutB1 extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <ScrollView style={styles.container}>
-        <Text style={styles.liftName}>T1 OHP</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3+' />
-        </View>
-
-        <Text style={styles.liftName}>T2 Deadlift</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='10' />
-          <SetButton reps='10' />
-          <SetButton reps='10' />
-        </View>
-
-        <Text style={styles.liftName}>T3 Dumbbell Row</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='15' />
-          <SetButton reps='15' />
-          <SetButton reps='15+' />
-        </View>
+        <Lift tier='T1' repScheme='1' exercise='OHP' />
+        <Lift tier='T2' repScheme='1' exercise='Deadlift' />
+        <Lift tier='T3' repScheme='1' exercise='Dumbbell Row' />
 
         <Button
           onPress={() => navigate('A2')}
@@ -155,28 +185,9 @@ class WorkoutA2 extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <ScrollView style={styles.container}>
-        <Text style={styles.liftName}>T1 Bench</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3+' />
-        </View>
-
-        <Text style={styles.liftName}>T2 Squat</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='10' />
-          <SetButton reps='10' />
-          <SetButton reps='10' />
-        </View>
-
-        <Text style={styles.liftName}>T3 Lat Pulldown</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='15' />
-          <SetButton reps='15' />
-          <SetButton reps='15+' />
-        </View>
+        <Lift tier='T1' repScheme='1' exercise='Bench' />
+        <Lift tier='T2' repScheme='1' exercise='Squat' />
+        <Lift tier='T3' repScheme='1' exercise='Lat Pulldown' />
 
         <Button
           onPress={() => navigate('B2')}
@@ -195,28 +206,9 @@ class WorkoutB2 extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <ScrollView style={styles.container}>
-        <Text style={styles.liftName}>T1 Deadlift</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3' />
-          <SetButton reps='3+' />
-        </View>
-
-        <Text style={styles.liftName}>T2 OHP</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='10' />
-          <SetButton reps='10' />
-          <SetButton reps='10' />
-        </View>
-
-        <Text style={styles.liftName}>T3 Dumbbell Row</Text>
-        <View style={styles.setButtonContainer}>
-          <SetButton reps='15' />
-          <SetButton reps='15' />
-          <SetButton reps='15+' />
-        </View>
+        <Lift tier='T1' repScheme='1' exercise='Deadlift' />
+        <Lift tier='T2' repScheme='1' exercise='OHP' />
+        <Lift tier='T3' repScheme='1' exercise='Dumbbell Row' />
 
         <Button
           onPress={() => navigate('A1')}
